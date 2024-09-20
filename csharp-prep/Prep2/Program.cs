591cdfa019faa36b6c2ad31ecc7e42a47960f3a4
Siulenb <nav21001@byui.edu>
2024-09-20 17:46:19 -0600

    finished pre 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage grade? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string yourGrade = " ";
+
+        if (grade >= 90)
+        {
+            yourGrade = "A";
+            // Console.WriteLine($"Congratulations! You have passed with {yourGrade}");
+        }
+        else if (grade >= 80)
+        {
+            yourGrade = "B";
+            // Console.WriteLine($"Congratulations! You have passed with {yourGrade}");
+        }
+        else if (grade >= 70)
+        {
+            yourGrade = "C";
+            // Console.WriteLine($"Congratulations! You have passed with {yourGrade}");
+        }
+        else if (grade >= 60)
+        {
+            yourGrade = "D";
+            // Console.WriteLine($"Sorry, you can do it better next time: {yourGrade}");
+        }
+        else if (grade <= 60)
+        {
+            yourGrade = "F";
+            // Console.WriteLine($"Sorry, you can do it better next time: {yourGrade}");
+        }
+
+        //Stretch
+        string symbol = " ";
+        int lastDigit;
+        lastDigit = grade % 10;
+        if (lastDigit >= 7)
+        {
+            symbol = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            symbol = "-";
+        }
+        else
+        {
+            symbol = "";
+        }
+
+        if (grade >= 94 || grade < 60)
+        {
+            Console.WriteLine($"Your grade is: {yourGrade}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is: {yourGrade}{symbol}");
+        }
+        
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congratulations! You have passed.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, You can do it better next time.");
+        }
     }
 }
