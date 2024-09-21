dc549e220700730b6424b91591c9e95652bb6d59
Siulenb <nav21001@byui.edu>
2024-09-21 14:37:10 -0600

    finished assignment 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Declare the list of numbers
+        List<int> numbers = new List<int>();
+
+        // variable
+        bool quit = false;
+        int count = 0;
+        int largestNumber = 0;
+        int smallestNumber = 99999999;
+        int total = 0;
+
+        // Intro message
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (quit != true)
+        {
+            Console.Write("Enter number: ");
+            int enterNumber = int.Parse(Console.ReadLine());
+
+            if (largestNumber < enterNumber)
+            {
+                largestNumber = enterNumber;
+            }
+            if (smallestNumber > enterNumber && enterNumber > 0)
+            {
+                smallestNumber = enterNumber;
+            }
+            if (enterNumber != 0)
+            {
+                numbers.Add(enterNumber);
+            }
+            else if (enterNumber == 0)
+            {
+                quit = true;
+                count--;
+            }
+            count++; // count the list numbers.count;
+        }
+        // looping to sum
+        foreach (int sumNumber in numbers)
+        {
+            total += sumNumber;
+        }
+
+        // The average number
+        float average = ((float)total) / count;
+
+        // Results
+        Console.WriteLine($"The total sum of the list is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest number is: {smallestNumber}");
+        
+
     }
 }
