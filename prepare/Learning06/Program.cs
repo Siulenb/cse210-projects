3d7dd239536a47d363c9f03ca4e52e0a5a8a9b8b
Siulenb <nav21001@byui.edu>
2024-10-23 22:12:41 -0600

    done

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning06 World!");
+
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Blue", 5);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("green", 5, 2);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Red", 6);
+        shapes.Add(shape3);
+
+        foreach (Shape i in shapes)
+        {
+            string color = i.GetColor();
+
+            double area = i.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {Math.Round(area,2)}.");
+
+
+        }
+
     }
 }
---
+++ prepare/Learning06/Shape.cs
@@ -0,0 +1,32 @@
+
+
+
+public abstract class Shape
+{
+
+    private string _color;
+
+    public Shape (string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    // public double GeArea()
+    // {
+    //     return 0;
+
+    // }
+
+    public abstract double GetArea();
+
+}
